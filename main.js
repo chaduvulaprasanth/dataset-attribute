9cd9982960febb5a8c3e68cdfdc5f01df5f0ae13
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2019-08-28 22:07:03 +0530

    updated practices files on dataset attribute

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const el = document.querySelector('#user');
 
 el.id == 'user';
-// el.dataset.id === '1234567890';
+el.dataset.id = '1234567890';
 el.dataset.id = "456";
-// el.dataset.user === 'johndoe'
-// el.dataset.dateOfBirth === ''
+// result of date of birth
+el.dataset.dateOfBirth === ''
 
 // set the data attribute
-// el.dataset.dateOfBirth = '1960-10-03'; 
+el.dataset.dateOfBirth = '28-07-1998';
 // Result: el.dataset.dateOfBirth === 1960-10-03
 
-// delete el.dataset.dateOfBirth;
+delete el.dataset.dateOfBirth;
 // // Result: el.dataset.dateOfBirth === undefined
 
-// // 'someDataAttr' in el.dataset === false
-// el.dataset.someDataAttr = 'mydata';
-// // Result: 'someDataAttr' in el.dataset === true
+'someDataAttr' in el.dataset === false
+el.dataset.someDataAttr = 'mydata';
+// Result: 'someDataAttr' in el.dataset === true
